41c081d8dc9b69ba51fb56c1e4fa76b760bddbad
Elchibek <abdylashymov@gmail.com>
2024-09-04 16:34:42 +0600

    musna_lesson added

---
+++ lib/sabak01.dart
@@ -0,0 +1,41 @@
+import 'package:sabak01/sabak01.dart' as sabak01;
+
+void main() {
+  print(calculate());
+  var text = "Salam";
+  var text2 = "Salam aleikum";
+  var text3 = "Kandaisyn";
+
+  var san = 10;
+  var san2 = 5;
+  var san4 = 9;
+
+  String san1 = "14";
+  String san3 = "text";
+  String san5 = "text2";
+
+  int a = 1;
+  int b = -2;
+  int c = 0;
+
+  double bolchok = 9.0;
+  double bolchoksan = 4.9;
+  double bolchoktuu = 5.2;
+
+  num cifra = 14.1;
+  num cifra2 = 8;
+  num cifra3 = 4.6;
+
+  bool tuura = true;
+  bool kata = false;
+
+  dynamic ozgormo = "Salam";
+  ozgormo = 4;
+  ozgormo = true;
+  ozgormo = 6.8;
+  ozgormo = 4.4;
+}
+
+int calculate() {
+  return 6 * 7;
+}
